f77a840abf37abdc88c5dbb3ad0ae105fede1cdf
Wellinator <wellcoj@gmail.com>
2024-03-26 16:35:33 -0300

    fix(test): add memory to tests and update usage

--- EmulatorTests/CUPTests.cs
+++ EmulatorTests/CUPTests.cs
@@ -7,14 +7,16 @@ public class CPUTests
     [Fact]
     public void test_0xa9_lda_immediate_load_data()
     {
-        CPU uut = new CPU();
+        Memory mem = new Memory();
+        CPU uut = new CPU(mem);
         byte[] data = new byte[] { 0xa9, 0x05, 0x00 };
         uut.register_a = 10;
-        uut.interprete(data);
+
+        uut.loadAndRun(data);
+
 
         // Testing operation
         Assert.Equal(0x05, uut.register_a);
-
         // Testing status
         Assert.Equal(0b00, 0b00000010 & uut.status);
         Assert.Equal(0, 0b10000000 & uut.status);
@@ -23,10 +25,11 @@ public class CPUTests
     [Fact]
     public void test_0xa9_lda_zero_flag()
     {
-        CPU uut = new CPU();
+        Memory mem = new Memory();
+        CPU uut = new CPU(mem);
         byte[] data = new byte[] { 0xa9, 0x00, 0x00 };
 
-        uut.interprete(data);
+        uut.loadAndRun(data);
 
         // Testing status
         Assert.Equal(0b10, 0b00000010 & uut.status);
@@ -35,11 +38,16 @@ public class CPUTests
     [Fact]
     public void test_0xaa_tax_move_a_to_x()
     {
-        CPU uut = new CPU();
+        Memory mem = new Memory();
+        CPU uut = new CPU(mem);
         byte[] data = new byte[] { 0xaa, 0x00 };
+
+        uut.load(data);
+        uut.reset();
+
         uut.register_a = 10;
 
-        uut.interprete(data);
+        uut.run();
 
         Assert.Equal(10, uut.register_x);
     }
@@ -47,10 +55,11 @@ public class CPUTests
     [Fact]
     public void test_5_ops_working_together()
     {
-        CPU uut = new CPU();
+        Memory mem = new Memory();
+        CPU uut = new CPU(mem);
         byte[] data = new byte[] { 0xa9, 0xc0, 0xaa, 0xe8, 0x00 };
 
-        uut.interprete(data);
+        uut.loadAndRun(data);
 
         Assert.Equal(0xc1, uut.register_x);
     }
@@ -58,11 +67,17 @@ public class CPUTests
     [Fact]
     public void test_inx_overflow_should_wrap_to_zero()
     {
-        CPU uut = new CPU();
-        uut.register_x = 0xFF;
+        Memory mem = new Memory();
+        CPU uut = new CPU(mem);
         byte[] data = new byte[] { 0xe8, 0xe8, 0x00 };
 
-        uut.interprete(data);
+        uut.load(data);
+        uut.reset();
+
+        uut.register_x = 0xFF;
+
+        uut.run();
+
 
         Assert.Equal(0x01, uut.register_x);
     }
