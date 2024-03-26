3f1d2c9d32e1f86e1bc2c7a9d27c236a238e0c81
Wellinator <wellcoj@gmail.com>
2024-03-26 09:35:12 -0300

    feat: added CPU class

---
+++ Emulator/CPU/CPU.cs
@@ -0,0 +1,82 @@
+namespace NES_Emulator
+{
+    enum CPU_OPCODES
+    {
+        BRK = 0x00,
+        LDA = 0xA9,
+    }
+
+    public interface iCPU
+    {
+        public byte register_a { get; set; }
+        public byte register_x { get; set; }
+        public byte status { get; set; }
+        public byte program_counter { get; set; }
+
+        public void interprete(byte[] program);
+    }
+
+
+    public class CPU : iCPU
+    {
+        public CPU()
+        {
+            register_a = 0;
+            register_x = 0;
+            status = 0;
+            program_counter = 0;
+        }
+
+        public byte register_a { get; set; }
+        public byte register_x { get; set; }
+        public byte status { get; set; }
+        public byte program_counter { get; set; }
+
+        public void interprete(byte[] program)
+        {
+            program_counter = 0;
+
+            while (true)
+            {
+                byte opcode = program[program_counter];
+                program_counter++;
+
+                switch (opcode)
+                {
+                    case (byte)CPU_OPCODES.LDA:
+                        byte param = program[program_counter];
+                        program_counter++;
+                        register_a = param;
+
+                        if (register_a == 0)
+                        {
+                            status |= 0b00000010;
+                        }
+                        else
+                        {
+                            status &= 0b11111101;
+                        }
+
+                        if ((register_a & 0b10000000) != 0)
+                        {
+                            status |= 0b10000000;
+                        }
+                        else
+                        {
+                            status &= 0b01111111;
+                        }
+
+                        break;
+
+                    case (byte)CPU_OPCODES.BRK:
+                        return;
+
+                    default:
+                        return;
+                }
+
+            }
+        }
+    }
+
+}
