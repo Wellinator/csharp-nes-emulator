630e535c4c4b160d3b6384a8d5463ae960a42673
Wellinator <wellcoj@gmail.com>
2024-03-31 13:12:53 -0300

    test: added tests for BVS opcode

--- EmulatorTests/CUPTests.cs
+++ EmulatorTests/CUPTests.cs
@@ -356,4 +356,30 @@ public class CPUTests
 
         Assert.Equal(0x00, uut.register_acc);
     }
+
+    [Fact]
+    [Trait("Category", "Branch")]
+    public void test_branch_if_overflow_set()
+    {
+        byte[] data = new byte[] { 0x70, 0x02, 0x00, 0xa9, 0x05, 0x00 };
+
+        uut.load(data);
+        uut.reset();
+
+        uut.setStatus(CPUStatus.Overflow);
+        uut.run();
+        
+        Assert.Equal(0x05, uut.register_acc);
+    }
+
+    [Fact]
+    [Trait("Category", "Branch")]
+    public void test_not_branch_if_overflow_clear()
+    {
+        byte[] data = new byte[] { 0x70, 0x02, 0x00, 0xa9, 0x05, 0x00 };
+
+        uut.loadAndRun(data);        
+
+        Assert.Equal(0x00, uut.register_acc);
+    }
 }
